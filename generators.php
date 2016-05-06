b7c539be8a43bcc5f1a55fccb8a2d9ac462521d2
Godfrey Majwega <majregor@gmail.com>
2016-05-06 17:13:07 -0400

    Initial Commit
    Initial
    Generators Sample

---
+++ generators.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: godfreymajwega
+ * Date: 5/6/16
+ * Time: 4:29 PM
+ */
+
+/**
+ * Generator function to yield prime numbers between $start and $end
+ * @param int $start
+ * @param int $end
+ */
+function prime_numbers($start=1, $end=100){
+    for($i=$start; $i<=$end; $i++){
+        $is_prime = true;
+        for($j=2;$j<$i;$j++){
+            if($i%$j == 0){
+                $is_prime = false;
+                break;
+            }
+        }
+        if($is_prime && $i>1){
+            yield $i;
+        }
+    }
+}
+
+foreach(prime_numbers(1, 120) as $prime_number){
+    echo("<p> $prime_number </p>");
+}
